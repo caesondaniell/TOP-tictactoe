4f17d01e2b2af10d889b4949b88456cf9d34bf16
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-02-24 11:28:52 -0800

    Refactor game
    game no longer interacts with the DOM at all.
    Removed play().
    Began refactoring domControl.
    
    Next commit:
    -fix issue: turn assignment

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//RETURN board, players, startGame, startRound, claimSpace, checkStatus*
 const game = (() => {
+    //EXTERNAL
     const board = (() => {
         const grid = [
         "",
@@ -18,77 +20,75 @@ const game = (() => {
 
         return { grid, clear };
     })();
-
-    let playerList = [];
-
-    function newGame() {
+    //EXTERNAL
+    let players = (() => {
+        let list = [];
+        return { list };
+    })();
+    //EXTERNAL: clear board and reset players.list w/ two new players
+    function startGame() {
         board.clear();
-        playerList = [];
+        players.list = [];
         const player1 = newPlayer();
         const player2 = newPlayer();
         setMarker(player1);
         player2.marker = player1.marker === "X" ? "O" : "X";
-        playerList.push(player1, player2);
-        domController.updatePlayers(player1, player2);
+        players.list.push(player1, player2);
+        coinFlip();
     }
-
-    function play() {
-        const gameText = document.querySelector(".game-text");
-        const [player1, player2] = coinFlip();
-        while (!gameStatus().outcome) {
-            const currPlayer = whoseTurn(player1, player2);
-            gameText.textContent = `Next: ${currPlayer.name} (${currPlayer.marker})`;
-            claim(currPlayer);
-        }
-        gameText.textContent.concat(`\nGame over.`);
-        domController.updatePlayers(playerList[0], playerList[1]);
-        if (confirm("Play another round?")) newGame();
-    }
-
-    function newRound() {
-        board.clear();
-        if (confirm("Switch markers?")) {
-            const [player1, player2] = playerList;
-            [player1.marker, player2.marker] = [player2.marker, player1.marker];
-        }
-        play();
-    }
-
+    //INTERNAL (newGame): RETURN player objects
     function newPlayer() {
-        let marker, score = 0;
+        let turn = 0, marker, score = 0;
         const player = prompt("What's your name?");
         const name = player[0].toUpperCase()+player.slice(1).toLowerCase();
         const getScore = () => score;
         const addWin = () => { score++ };
-        return { name, marker, getScore, addWin };
+        return { name, marker, turn, getScore, addWin };
     }
-
+    //INTERNAL (newGame): set markers on player objects
     function setMarker(player) {
         player.marker = prompt(`Will ${player.name} play X or O?`).toUpperCase();
         while (player.marker !== "X" && player.marker !== "O") {
             player.marker = prompt(`Invalid entry. Choose X or O, please.`).toUpperCase();
         }
     }
-
-    function whoseTurn(one, two) {
-        const oneCount = board.grid.filter(space => space === one.marker);
-        const twoCount = board.grid.filter(space => space === two.marker);
-        const currPlayer = oneCount.length === twoCount.length ? one : two;
+    //EXTERNAL: clear board and optionally switch player markers
+    function startRound() {
+        board.clear();
+        if (confirm("Switch markers?")) {
+            const [player1, player2] = players.list;
+            [player1.marker, player2.marker] = [player2.marker, player1.marker];
+        }
+        coinFlip();
+    }
+    //INTERNAL (newGame, newRound): set turn order in player objects
+    function coinFlip() {
+        players.list[0].turn = 0;
+        players.list[1].turn = 0;
+        const flip = Math.floor(Math.random() * 2);
+        players.list[0].turn = flip === 0 ? 1 : 2;
+        players.list[1].turn = players.list[0].turn = 1 ? 2: 1;
+    }
+    //INTERNAL (claim, gameStatus): RETURN current player
+    function whoseTurn() {
+        const [player1, player2] = players.list[0].turn < players.list[1].turn ?
+                                    players.list : [players.list[1], players.list[0]];
+        const oneCount = board.grid.filter(space => space === player1.marker);
+        const twoCount = board.grid.filter(space => space === player2.marker);
+        const currPlayer = oneCount.length === twoCount.length ? player1 : player2;
         return currPlayer;
     }
-
-    function claim(player) {
-        let square = prompt(`${player.name}: Claim a square (1-9)`);
-        while (!(board.grid[square - 1] === "")) {
-            square = prompt(`Sorry ${player.name}, that square is claimed.\nClaim a square (1-9)`)
-        }
-        board.grid.splice(square - 1, 1, player.marker);
-        domController.updateGrid();
+    //EXTERNAL: place current player's marker in chosen space
+    function claimSpace(space) {
+        const currPlayer = whoseTurn();
+        if (board.grid[space] === "") {
+            board.grid.splice(space, 1, currPlayer.marker);
+        } else alert(`Whoopsie! That space is already claimed.`);
         console.log(board.grid);
     };
-
-    function gameStatus() {
-        let h = 0, outcome, winMark;
+    //EXTERNAL: check game status, RETURN outcome and display text
+    function checkStatus() {
+        let h = 0, gameText, outcome, winMark;
         while (h < 7) {
             if (board.grid[h] !== "" &&
                 board.grid[h] === board.grid[h + 1] &&
@@ -118,93 +118,79 @@ const game = (() => {
         }
         if (!board.grid.includes("")) outcome = "cat's game";
         if (outcome) {
-            const gameText = document.querySelector(".game-text");
             if (outcome === "cat's game") {
-                gameText.textContent = `It's a ${outcome}!`;
+                gameText = `Game over. It's a ${outcome}!`;
             } else {
                 let victor
-                playerList.forEach(player => {
+                players.list.forEach(player => {
                     if (winMark === player.marker) victor = player;
                 })
-                gameText.textContent = `It's a ${outcome}; ${victor.name} wins!`;
+                gameText = `Game over. It's a ${outcome}; ${victor.name} wins!`;
                 victor.addWin();
             }
+        } else {
+            const player = whoseTurn();
+            gameText = `${player.name} (${player.marker}), claim a space.`
         }
-        return { outcome };
+        return { outcome, gameText }; 
     }
 
-    function coinFlip() {
-        const flip = Math.floor(Math.random() * 2);
-        const [player1, player2] = playerList;
-        const [firstPlayer, secondPlayer] = flip === 0 ?
-                                [player1, player2] :
-                                [player2, player1];
-        return [firstPlayer, secondPlayer];
-    }
-
-    return { board, newGame, play };
+    return { 
+        board, 
+        players, 
+        startGame, 
+        startRound, 
+        claimSpace, 
+        checkStatus 
+    };
 })();
 
-const domController = (() => {
+const domControl = (() => {
+    const gameText = document.querySelector(".game-text");
+    const controlBtns = document.querySelectorAll(".game-control");
+    //Add functions to control buttons to start a new game or round
+    controlBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            switch (btn.classList[1]) {
+                case ("new"):
+                    controlBtns[0].toggleAttribute("hidden");
+                    controlBtns[1].toggleAttribute("hidden");
+                    game.startGame();
+                    updatePlayers();
+                    gameText.textContent = game.checkStatus().gameText;
+                    break;
+                case ("reset"):
+                    game.startRound();
+                    gameText.textContent = game.checkStatus().gameText;
+                    break;
+            }
+        })
+    });
+    //Render the board
     function updateGrid() {
         const btns = document.querySelectorAll(".grid-square");
         for (let i = 0; i < btns.length; i++) {
             btns[i].textContent = game.board.grid[i];
         }
     }
-
-    function updatePlayers(player1, player2) {
+    //Render each players' name, marker, score
+    function updatePlayers() {
+        const [player1, player2] = game.players.list;
         const p1Name = document.querySelector(".p1-name");
         const p1Mark = document.querySelector(".p1-mark");
         const p1Score = document.querySelector(".p1-score");
         const p2Name = document.querySelector(".p2-name");
         const p2Mark = document.querySelector(".p2-mark");
         const p2Score = document.querySelector(".p2-score");
-        p1Name.textContent = player1.name;
-        p1Mark.textContent = `marker: ${player1.marker}`;
-        p1Score.textContent = `score: ${player1.getScore()}`;
-        p2Name.textContent = player2.name;
-        p2Mark.textContent = `marker: ${player2.marker}`;
-        p2Score.textContent = `score: ${player2.getScore()}`;
-    }
-    
-    function elementCreator(element) {
-        return () => {
-            return document.createElement(element);
-        }
+        console.log(player1);
+        console.log(player2);
+        // p1Name.textContent = player1.name;
+        // p1Mark.textContent = `marker: ${player1.marker}`;
+        // p1Score.textContent = `score: ${player1.getScore()}`;
+        // p2Name.textContent = player2.name;
+        // p2Mark.textContent = `marker: ${player2.marker}`;
+        // p2Score.textContent = `score: ${player2.getScore()}`;
     }
 
     return { updateGrid, updatePlayers };
 })();
-
-const control = document.querySelector(".game-control");
-control.addEventListener("click", () => {
-    switch (control.textContent) {
-        case ("new game"):
-            game.newGame();
-            control.textContent = "play round";
-            break;
-        case ("play round"):
-            game.play();
-            control.textContent = "clear board";
-            break;
-        case ("clear board"):
-            game.board.clear();
-            domController.updateGrid();
-            game.play();
-            break;
-    }
-})
-
-
-// for testing
-// board.claim(1, "X");
-// board.claim(2, "O");
-// board.claim(5, "X");
-// board.claim(8, "O");
-// board.claim(9, "X");
-// board.claim(9, "O");
-// board.claim(5, "X");
-// board.claim(7, "O");
-// board.claim(8, "X");
-// board.clear();
